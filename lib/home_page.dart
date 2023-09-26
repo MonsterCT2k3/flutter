6fb19a321d21dcd4e29b3030239e1a418c5698be
monsterct-admin <monkeyd.nam2003@gmail.com>
2023-09-26 10:56:39 +0700

    animated button pressed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:init_project/new_button.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isPressed = false;
+  void onPressed() {
+    setState(() {
+      if (isPressed) {
+        isPressed = false;
+      } else {
+        isPressed = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[300],
-      body: const Center(
-        child: NewButton(),
+      body: Center(
+        child: NewButton(
+          onPressed: onPressed,
+          isPressed: isPressed,
+        ),
       ),
     );
   }
--- lib/new_button.dart
+++ lib/new_button.dart
@@ -1,39 +1,59 @@
 import 'package:flutter/material.dart';
 
 class NewButton extends StatelessWidget {
-  const NewButton({super.key});
+  const NewButton({
+    super.key,
+    required this.onPressed,
+    required this.isPressed,
+  });
+  final void Function() onPressed;
+  final bool isPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180,
-      width: 180,
-      decoration: BoxDecoration(
-        color: Colors.deepPurple[300],
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.deepPurple.shade700,
-            offset: const Offset(4, 4),
-            blurRadius: 15,
-            spreadRadius: 1,
+    return GestureDetector(
+      onTap: onPressed,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 50),
+        height: 180,
+        width: 180,
+        decoration: BoxDecoration(
+          color: Colors.deepPurple[300],
+          //shape: BoxShape.circle,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+              color: isPressed ? Colors.grey.shade200 : Colors.grey.shade400),
+          boxShadow: isPressed
+              ? []
+              : [
+                  BoxShadow(
+                    color: Colors.deepPurple.shade700,
+                    offset: const Offset(4, 4),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+                  BoxShadow(
+                    color: Colors.deepPurple.shade200,
+                    offset: const Offset(-4, -4),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+                ],
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade400,
+            ],
+            stops: const [
+              0.1,
+              0.9,
+            ],
           ),
-          BoxShadow(
-            color: Colors.deepPurple.shade200,
-            offset: const Offset(-4, -4),
-            blurRadius: 15,
-            spreadRadius: 1,
-          ),
-        ],
-        gradient: LinearGradient(
-          colors: [
-            Colors.deepPurple.shade200,
-            Colors.deepPurple.shade400,
-          ],
-          stops: const [
-            0.1,
-            0.9,
-          ],
+        ),
+        child: Icon(
+          Icons.favorite,
+          color: isPressed ? Colors.red.shade300 : Colors.red,
+          size: 120,
         ),
       ),
     );
